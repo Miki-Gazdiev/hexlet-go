acb4110c8fd87a543d0633ea833c8d0550b8562a
Miki <mikigazdiev@yandex.ru>
2026-06-18 14:53:51 +0300

    module

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"github.com/fatih/color"
 	"hexlet-go/greeting"
 	"fmt"
 )
 
 func main() {
 	fmt.Println(greeting.Hello())
+	color.Blue("Prints %s in blue.", "text")
 }
